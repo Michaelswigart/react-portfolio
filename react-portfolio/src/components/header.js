74dcc8e35b0f371eb7ad99bb904102e1335b5533
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-04-30 23:50:24 -0500

    update.1.4 css

--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -1,15 +1,16 @@
-//import { Link } from "react-router-dom";
+import React from "react";
+//import mylogo"../logo.png;
 
 function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  <a className="navbar-brand" href="#d">Welcome</a>
+  <a className="navbar-brand" href="#d"><img className="logo" src="{logo}" alt="logo..."/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  
+
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>

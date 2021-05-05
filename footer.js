7050e6d6f8c464eab348549e1a4b64e5c30a2a85
Michaelswigart <sherlockholmes.swigart92@gmail.com>
2021-05-04 19:09:09 -0700

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Footer = ()=> {
+    
+      return (
+          <>
+  
+        <footer>
+          <nav className="navbar navbar-dark bg-dark fixed-bottom"><a className="navbar-brand" href="https://www.facebook.com/alex.swigart.98"><img src="https://cdn2.iconfinder.com/data/icons/social-media-applications/64/social_media_applications_1-facebook-512.png" width="32px" alt="" />Welcome</a></nav>
+        </footer>
+      </>
+        );
+    
+  };
+  
+  export default Footer;
---
+++ portfolio.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+var Portfolio = ()=> {
+    
+      return (
+          <>
+        <div> 
+          <main className="">
+            <div className=" card body" style={{maxWidth: '25rem'}}>
+              <img src="https://user-images.githubusercontent.com/73671076/104788236-67ca1400-5757-11eb-914d-2bb0759a3f2e.png" className="rounded float-left" alt="" />
+              <a
+              classname="App-link"
+              href="https://github.com/Michaelswigart/I-know-your-past-present-and-future"
+              target="_blank"
+              rel="noopener noreferrer">see here
+
+              </a>
+              <img src="https://user-images.githubusercontent.com/73671076/107088057-ffa0a800-67c1-11eb-8e7e-d3a1f08299f2.png" className="rounded 
+              float-right" alt="" />
+              <a
+              className="App-link"
+              href="https://github.com/Michaelswigart/It-lives"
+              target="_blank"
+              rel="nooperner noreferrer">see here
+
+              </a>
+              <img src="https://user-images.githubusercontent.com/73671076/106238540-a4b9f000-61c6-11eb-81ec-b330e62964a1.png" className="rounded float-left" alt="" />
+              <a
+              className="App-link"
+              href="https://github.com/JarrettHeller/DnD-Spells"
+              target="_blank"
+              rel="nooperner noreferrer">see here
+
+              </a>
+              <img src=" https://user-images.githubusercontent.com/73671076/107866483-25b7ef00-6e37-11eb-937c-8367a3cbe123.png" className="rounded float-left" alt="readme generator" />
+              <a
+              className="App-link"
+              href="https://github.com/Michaelswigart/Professional-README-Generator"
+              target="_blank"
+              rel="nooperner noreferrer">see here
+
+              </a>
+              <img src="https://user-images.githubusercontent.com/73671076/108798903-8391b880-7554-11eb-9a23-cce15cc7555c.png" className="rounded float-left" alt="team profile generator" />
+              <a
+              className="App-link"
+              href="https://github.com/Michaelswigart/Employee-Tracker"
+              target="_blank"
+              rel="nooperner noreferrer">see here
+
+              </a>
+              {/* <img src="images\task-project-2 (1).png" className="rounded float-left" alt="Projct 2" />
+              <a
+              className="App-link"
+              href="https://github.com/Keaton-Brewster/Routinify"
+              target="_blank"
+              rel="nooperner noreferrer">having problem with image so please click here to see more
+
+              </a> */}
+            </div>
+          </main>  
+        </div>
+    </>  );
+    
+  };
+
+  export default Portfolio;
